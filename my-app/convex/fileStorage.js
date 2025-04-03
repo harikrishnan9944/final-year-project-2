90e77cee6a53206cbfc58e3d220806cc6c8fe909
harikrishnan9944 <148446498+harikrishnan9944@users.noreply.github.com>
2025-04-03 19:33:36 +0530

    Add files via upload

---
+++ my-app/convex/fileStorage.js
@@ -0,0 +1,58 @@
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const generateUploadUrl = mutation(async (ctx) => {
+  return await ctx.storage.generateUploadUrl();
+});
+
+export const AddFileEntryToDb = mutation({
+  args: {
+    fileId: v.string(),
+    storageId: v.string(), 
+    fileName: v.string(),
+    createdBy: v.string(),
+    fileUrl:v.string()
+  },
+  handler: async (ctx, args) => {
+    const result = await ctx.db.insert("pdfFile", {
+      fileId: args.fileId,
+      fileName: args.fileName,
+      storageId: args.storageId,
+      createdBy: args.createdBy,
+      fileUrl:args.fileUrl
+    });
+    return "Inserted";
+  },
+});
+
+export const getFileUrl=mutation({
+  args:{
+    storageId:v.string(),
+  },
+  handler:async(ctx,args)=>{
+    const url=await ctx.storage.getUrl(args.storageId);
+    return url;
+  }
+})
+
+
+export const GetFileRecord=query({
+  args:{
+fileId:v.string()
+  },
+  handler:async(ctx,args)=>{
+   const result= await ctx.db.query('pdfFile').filter((q)=>q.eq(q.field('fileId'),args.fileId))
+    .collect();
+    // console.log(result)
+    return result[0];
+  }
+})
+
+
+
+
+// Query to get ALL PDF files
+
+
+
+
